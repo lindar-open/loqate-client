bd327073b1086f2b2f5831dcc6858b7368ee7265
Adam Wojdecki <adam@mrq.com>
2022-02-01 10:21:28 +0100

    added optional base-url when creating LoqateClient

--- src/main/java/com/lindar/loqate/util/LoqateConstants.java
+++ src/main/java/com/lindar/loqate/util/LoqateConstants.java
@@ -4,8 +4,9 @@ import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class LoqateConstants {
-    public static final String ADDRESS_VERIFICATION_FIND_ENDPOINT     = "https://api.addressy.com/Capture/Interactive/Find/v1.10/json3.ws";
-    public static final String ADDRESS_VERIFICATION_RETRIEVE_ENDPOINT = "https://api.addressy.com/Capture/Interactive/Retrieve/v1.00/json3.ws";
+    public static final String BASE_URL = "https://api.addressy.com";
+    public static final String ADDRESS_VERIFICATION_FIND_ENDPOINT     = "/Capture/Interactive/Find/v1.10/json3.ws";
+    public static final String ADDRESS_VERIFICATION_RETRIEVE_ENDPOINT = "/Capture/Interactive/Retrieve/v1.00/json3.ws";
 
-    public static final String EMAIL_VERIFICATION_ENDPOINT = "https://api.addressy.com/EmailValidation/Interactive/Validate/v2.00/json3.ws";
+    public static final String EMAIL_VERIFICATION_ENDPOINT = "/EmailValidation/Interactive/Validate/v2.00/json3.ws";
 }
